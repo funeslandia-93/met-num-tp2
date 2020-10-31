e21e827218cc8967b27338d4a1283fd45e311ddb
mfachal <647731+mfachal@users.noreply.github.com>
2020-10-31 17:42:56 -0300

    added entry point

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,39 @@
 #include <iostream>
 #include "pca.h"
 #include "eigen.h"
+#include <fstream>
 
 int main(int argc, char** argv){
 
-  std::cout << "Hola mundo!" << std::endl;
+	std::string line;
 
-  return 0;
+	PCA pca = new PCA(4);
+
+	std::vector<std::vector<unsigned int> > entries;
+
+	std::ifstream file (argv[1]); //a.out ../data/test.csv etc
+
+	getline(file, line); //skip labels
+
+	while (getline(file, line)){
+		std::vector<unsigned int> numbers;
+
+		//copied from SO
+		std::string delimiter = ",";
+		size_t pos = 0;
+		std::string token;
+		while ((pos = line.find(delimiter)) != std::string::npos) {
+			token = line.substr(0, pos);
+			numbers.push(stoi(token, nullptr));
+			line.erase(0, pos + delimiter.length());
+		}
+
+		entries.push(numbers);
+	}
+
+	//acá ejecutar PCA(entries) o KNN(entries), pero creo que hay que cambiar algo del tipo
+
+	pca.fit(entries)
+
+	return 0;
 }
